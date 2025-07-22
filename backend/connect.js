53aec7b6898eb2b1e9e60fcb5c1e04ee7b9fa047
Nirmal Fernando <fernandonirmal607@gmail.com>
2025-07-22 22:34:43 +0530

    Initial backend file structuring

---
+++ backend/connect.js
@@ -0,0 +1,38 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+// Load environment variables from the .env file
+dotenv.config();
+
+// Create a new Sequelize instance and configure the database connection
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 5432,
+    logging: true,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+
+// Test the connection to ensure it works
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+testConnection();
+
+export default sequelize;
