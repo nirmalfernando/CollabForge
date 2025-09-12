646cb91350c30b96ab379607de6f53246007cdd6
Lihindu Perera <lihindu.indudunu.perera@gmail.com>
2025-09-12 13:07:00 +0530

    missing prop

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -37,7 +37,7 @@ export default function LandingPage() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <Header />
+      <Header isLoggedIn={false} />
 
       <main className="flex-1">
         {/* Hero Section */}
